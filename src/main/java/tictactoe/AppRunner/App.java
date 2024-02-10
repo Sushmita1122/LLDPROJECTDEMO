2bc4b50f3bb9302db5824a9ea86511b0079f71ca
Sushmita <sushmitakaushal98@gmail.com>
2024-02-10 20:19:12 +0530

    Tic tac toe project1

--- src/main/java/tictactoe/AppRunner/App.java
+++ src/main/java/tictactoe/AppRunner/App.java
@@ -13,7 +13,6 @@ import tictactoe.winningstrategies.WinningStrategy;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
 public class App {
 
     public static void main(String[] args) throws DuplicateSymbolException, PlayersCountMismatchException, MoreThanOneBotException {
